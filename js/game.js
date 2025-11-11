c8d0a22f08123d9acccfe379a6faa3e44faaa4c4
quewon <36914642+quewon@users.noreply.github.com>
2025-11-11 20:18:27 +0100

    trying to fix svgs + dont break if path doesnt work

--- js/game.js
+++ js/game.js
@@ -188,7 +188,7 @@ class Game {
             this.updateElementTransform(element);
         }
 
-        for (let svg of document.querySelectorAll("svg")) {
+        for (let svg of document.querySelectorAll(".fh-slide.open > .fh-element svg")) {
             const element = svg.closest(".fh-element");
             if (!element) continue;
             const transform = element.style.transform;
@@ -301,7 +301,10 @@ class Game {
         }
 
         const previousSlide = this.currentSlide;
-        this.currentSlide = this.getElementAtPath(path);
+        try {
+            this.currentSlide = this.getElementAtPath(path);
+        }
+        catch { }
 
         for (let open of document.querySelectorAll(".open"))
             open.classList.remove("open");
@@ -372,24 +375,7 @@ class Game {
         }
 
         if (this.cachedGameRect) {
-            for (let element of document.querySelectorAll(".fh-slide.open > .fh-element")) {
-                this.updateElementTransform(element);
-            }
-
-            for (let svg of this.currentSlide.querySelectorAll("svg")) {
-                const element = svg.closest(".fh-element");
-                if (!element) continue;
-                const transform = element.style.transform;
-                element.style.transform = "";
-                svg.style.display = 'none';
-                svg.offsetHeight;
-                requestAnimationFrame(() => {
-                    svg.style.display = '';
-                    requestAnimationFrame(() => {
-                        element.style.transform = transform;
-                    })
-                })
-            }
+            this.onresize();
         }
     }
 
